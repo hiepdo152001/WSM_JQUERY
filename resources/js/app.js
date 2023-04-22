9ff348a9a590044ba883a1c8242e1e602ce7da33
hiepdv <hiep.dv@zinza.com.vn>
2023-04-22 22:46:07 +0700

    thay doi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,2 @@
 require('./bootstrap');
 
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import App from './App.vue';
-import Home from './components/Home.vue';
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            components: Home
-        },
-    ]
-});
-
-new Vue({
-    el: '#app',
-    router: router,
-    render: app => app(App)
-});
