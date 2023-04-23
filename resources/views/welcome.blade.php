c776bb9ea5ed2cee579f65e8eaf8cef877898993
hiepdv <hiep.dv@zinza.com.vn>
2023-04-23 11:37:19 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -13,5 +13,18 @@
         <div id="app"></div>
     </div>
     <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html> --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div id="app"></div>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
