94a1ee02e1eb9568c2c4044fb65fe908f2fab467
hiepdv <hiep.dv@zinza.com.vn>
2023-05-10 11:35:18 +0700

    logout(done)

--- resources/js/layouts/auth/logout.vue
+++ resources/js/layouts/auth/logout.vue
@@ -1,35 +1,36 @@
 <template>
-    <div>
-      <form>
-        <div class="row justify-content-center">
-                <div class="col-sm-4 mt-5">
-                    <form @submit.prevent="logout" >
-                        <span type="submit" class="btn ">Logout</span>
-                    </form>
-                </div>
-            </div>
-      </form>
-    </div>
-</template>
-<script>
-     import { reactive, ref, onMounted } from 'vue';
-     import { useRouter } from 'vue-router'
-     import axios from 'axios';
-     export default{
-      setup(){
-        let isAuthenticated =ref(true)
-        const router = useRouter()
-        const logout =async()=>{
-                    let res = await axios.get('api/auth/logout')
-             router.push("/")
-             router.go(0)
-                }
-      return {
-        logout,
-        isAuthenticated,
-      }
+  <div>
+    <form>
+      <div class="row justify-content-center">
+        
+        <div class="col-sm-4 mt-5">
+          <form @submit.prevent="logout" >
+            
+            <button type="submit" class="btn btn-dark">Logout</button>
+          </form>
+        </div>
+      </div>
+    </form>
+  </div>
+  </template>
+  <script>
+   import { reactive, ref, onMounted } from 'vue';
+   import { useRouter } from 'vue-router'
+   import axios from 'axios';
+   export default{
+    setup(){
+      const router = useRouter()
+      const logout =async()=>{
+          let res = await axios.get('api/auth/logout')
+          console.log(res.data)
+          if(res.data.status===true){
+            await router.push("/")
+          }
+        }
+    return {
+      logout,
     }
-     }
-      
-      
-</script>
+  }
+   }
+ 
+  </script>
