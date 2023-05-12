d3b324158d65913ca40363c08666ff298389063f
hiepdv <hiep.dv@zinza.com.vn>
2023-05-12 23:08:01 +0700

    migrate notify

--- database/migrations/2023_04_18_160710_contact.php
+++ database/migrations/2023_04_18_160710_contact.php
@@ -13,20 +13,20 @@ class Contact extends Migration
      */
     public function up()
     {
-        Schema::create('Contact' ,function(Blueprint $table){
-            $table -> id();
-            $table -> string('user_id');
-            $table -> timestamp('content');
-            $table -> integer('type');
-            $table -> integer('phone');
-            $table -> string('project');
-            $table -> string('reason');
-            $table -> timestamp('time_start');
-            $table -> timestamp('time_end');
-            $table -> integer('status');
-            $table -> timestamp('dealine');
-            $table -> timestamps();
-          });
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('content');
+            $table->integer('type');
+            $table->integer('phone');
+            $table->string('project');
+            $table->string('reason');
+            $table->timestamp('time_start')->nullable();
+            $table->timestamp('time_end')->nullable();
+            $table->integer('status');
+            $table->timestamp('dealine')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -36,6 +36,6 @@ class Contact extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Contact');
+        Schema::dropIfExists('contacts');
     }
 }
