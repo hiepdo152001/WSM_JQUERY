25bd59b06a1ca2dc0efddecd2d4e5149e9b5336d
hiepdv <hiep.dv@zinza.com.vn>
2023-05-26 09:13:27 +0700

    service get work time

--- app/Services/CalendarService.php
+++ app/Services/CalendarService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\Time_keep;
 use App\Models\User;
 use Date;
+use DateTime;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
@@ -42,10 +43,44 @@ class CalendarService
 
         $time_keep = Time_keep::where('user_id', $id)
             ->where('day', $dayCheck)
-
             ->first();
-
         $time_keep->time_out = $dateNow;
+        $time_keep->save();
+
+        $time_in = $time_keep->time_in;
+        $time_out = $time_keep->time_out;
+
+        $time_in_dt = new DateTime($time_in);
+        $time_out_dt = new DateTime($time_out);
+
+        $time13h = new DateTime();
+        $time12h = new DateTime();
+        $time13h->setTime(13, 0, 0);
+        $time13h->format('Y-m-d H:i:s');
+        $time12h->setTime(12, 0, 0);
+        $time12h->format('Y-m-d H:i:s');
+
+        $diff = $time_in_dt->diff($time_out_dt);
+        $h = $diff->h;
+        $i = $diff->i;
+        $result = $h  + $i / 60;
+
+        $work_time = round($result, 1);
+
+        if ($time_in_dt <= $time12h &&  $time13h <= $time_out_dt) {
+            $work_time -= 1;
+        } elseif ($time_in_dt <= $time12h &&  $time12h <= $time_out_dt  &&  $time_out_dt <= $time13h) {
+            $time = $time12h->diff($time_out_dt);
+            $time_redundant = $time->h + ($time->i / 60);
+            $work_time -= $time_redundant;
+        } elseif ($time12h <= $time_in_dt &&  $time_in_dt <= $time13h && $time_out_dt >= $time13h) {
+            $time = $time_in_dt->diff($time13h);
+            $time_redundant = $time->h + ($time->i / 60);
+            $work_time -= $time_redundant;
+        }
+        $work_time = round(($work_time / 8), 1);
+        $time_keep->work_time = $work_time;
+
         $time_keep->save();
 
         return $time_keep;
@@ -65,5 +100,14 @@ class CalendarService
             ->where('day', $day)
             ->where('month', $month_keep)
             ->first();
+    }
+
+    public function getWorkTime($id)
+    {
+        $dateNow = date('Y-m-d H:i:s');
+        $month_keep = date('m', strtotime('-1 month', strtotime($dateNow)));
+        return Time_keep::where('user_id', $id)
+            ->where('month', $month_keep)
+            ->sum('work_time');
     }
 }
