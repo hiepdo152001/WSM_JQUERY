5b3b07669c00ac31f702002598657ab8edcc0475
hiepdv <hiep.dv@zinza.com.vn>
2023-05-29 09:53:16 +0700

    add private route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,11 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import admin from './routes.js';
+import { createRouter, createWebHistory } from "vue-router";
+import admin from "./routes.js";
+import jwtService from "../layouts/common/jwtService.js";
 
 const routes = [...admin];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
-
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (jwtService.getToken()) {
+      next();
+    } else {
+      next("/");
+    }
+  } else {
+    next();
+  }
+});
 export default router;
--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,90 +1,94 @@
 const admin = [
-    {
-      path: "/",
-      component: () => import("../layouts/homePage.vue"),
-      children: [
-        {
-          path: "login",
-          name: "login-auth",
-          component: () => import("../layouts/homePage.vue"),
-        },
-        {
-          path: "register",
-          name: "register-auth",
-          component: () => import("../layouts/homePage.vue"),
-        },
-        
-      ],
+  {
+    path: "/",
+    component: () => import("../layouts/homePage.vue"),
+    meta: {
+      requiresAuth: false,
     },
-    {
-      path: "/home",
-      component: () => import("../layouts/homeWeb.vue"),
-      children: [
-        {
-          path: "lich",
-          name: "lich-lam-viec",
-          component: () => import("../layouts/user/calendar.vue"),
-        },
-        {
-          path: "request",
-          name: "yeu-cau-cua-toi",
-          component: () => import("../layouts/user/requestUser.vue"),
-        },
-        {
-          path: "request/new",
-          name: "new-request",
-          component: () => import("../layouts/user/createRequest.vue"),
-        },
-        {
-          path: "change-password",
-          name: "change-password-auth",
-          component: () => import("../layouts/auth/passWord.vue"),
-        },
-        {
-          path: "member/request",
-          name: "member-request",
-          component: () => import("../layouts/user/memberRequest.vue"),
-          children: [
-            {
-              path: "?type=pending",
-              name: "pending",
-              component: () => import("../layouts/user/requestStatus1.vue"),
-            },
-            {
-              path: "?type=confirmed",
-              name: "confirmed",
-              component: () => import("../layouts/user/requestStatus2.vue"),
-            },
-            {
-              path: "?type=approved",
-              name: "approved",
-              component: () => import("../layouts/user/requestStatus3.vue"),
-            },
-            {
-              path: "?type=declined",
-              name: "declined",
-              component: () => import("../layouts/user/requestStatus4.vue"),
-            },
-            {
-              path: "?type=canceled",
-              name: "canceled",
-              component: () => import("../layouts/user/requestStatus5.vue"),
-            },
-          ],
-        },
-        {
-          path: "user",
-          name: "user",
-          component: () => import("../layouts/user/user.vue"),
-        },
-        {
-          path: "edit/profile",
-          name: "edit-profile",
-          component: () => import("../layouts/user/editprofile.vue"),
-        },
-      ],
+    children: [
+      {
+        path: "login",
+        name: "login-auth",
+        component: () => import("../layouts/homePage.vue"),
+      },
+      {
+        path: "register",
+        name: "register-auth",
+        component: () => import("../layouts/homePage.vue"),
+      },
+    ],
+  },
+  {
+    path: "/home",
+    component: () => import("../layouts/homeWeb.vue"),
+    children: [
+      {
+        path: "lich",
+        name: "lich-lam-viec",
+        component: () => import("../layouts/user/calendar.vue"),
+      },
+      {
+        path: "request",
+        name: "yeu-cau-cua-toi",
+        component: () => import("../layouts/user/requestUser.vue"),
+      },
+      {
+        path: "request/new",
+        name: "new-request",
+        component: () => import("../layouts/user/createRequest.vue"),
+      },
+      {
+        path: "change-password",
+        name: "change-password-auth",
+        component: () => import("../layouts/auth/passWord.vue"),
+      },
+      {
+        path: "member/request",
+        name: "member-request",
+        component: () => import("../layouts/user/memberRequest.vue"),
+        children: [
+          {
+            path: "?type=pending",
+            name: "pending",
+            component: () => import("../layouts/user/requestStatus1.vue"),
+          },
+          {
+            path: "?type=confirmed",
+            name: "confirmed",
+            component: () => import("../layouts/user/requestStatus2.vue"),
+          },
+          {
+            path: "?type=approved",
+            name: "approved",
+            component: () => import("../layouts/user/requestStatus3.vue"),
+          },
+          {
+            path: "?type=declined",
+            name: "declined",
+            component: () => import("../layouts/user/requestStatus4.vue"),
+          },
+          {
+            path: "?type=canceled",
+            name: "canceled",
+            component: () => import("../layouts/user/requestStatus5.vue"),
+          },
+        ],
+      },
+      {
+        path: "user",
+        name: "user",
+        component: () => import("../layouts/user/user.vue"),
+      },
+      {
+        path: "edit/profile",
+        name: "edit-profile",
+        component: () => import("../layouts/user/editprofile.vue"),
+      },
+    ],
+    meta: {
+      requiresAuth: true,
     },
-  ];
-  
-  export default admin;
-  
+  },
+];
+
+export default admin;
