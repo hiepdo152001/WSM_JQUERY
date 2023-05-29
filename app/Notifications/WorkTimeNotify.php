80ea861b07b96837f2f6b719c0656a61d2ab69df
hiepdv <hiep.dv@zinza.com.vn>
2023-05-29 09:52:49 +0700

    implements should queue for work Time notify

--- app/Notifications/WorkTimeNotify.php
+++ app/Notifications/WorkTimeNotify.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class WorkTimeNotify extends Notification
+class WorkTimeNotify extends Notification implements ShouldQueue
 {
     use Queueable;
 
