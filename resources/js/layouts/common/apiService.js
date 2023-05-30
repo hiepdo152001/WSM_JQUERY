fe20c36ca814836597f4681f41401b93426f7e43
hiepdv <hiep.dv@zinza.com.vn>
2023-05-30 22:13:08 +0700

    url format , update api Service

--- resources/js/layouts/common/apiService.js
+++ resources/js/layouts/common/apiService.js
@@ -1,93 +1,179 @@
 import axios from "axios";
 import JwtService from "./jwtService";
-import { API_URL} from "./config"
-import { ref, onMounted } from 'vue';
+import { API_URL } from "./config";
+import { ref, onMounted } from "vue";
 
 export const axiosClient = axios.create({
-    baseURL: API_URL
+  baseURL: API_URL,
 });
 
-const AppService={
-    setHeader(){
-        return Headers={
-            'Authorization': `Bearer ${JSON.parse(JwtService.getToken())}`
-          
-        };
-    },
-    getTypeParameter(){
-        const url = window.location.href;
-          
-          const params = new URLSearchParams(window.location.search);
-          const type = params.get('type');
-          return type;
-    },
-    get(resource,params){
-        return axiosClient.get(`${resource}`,params);
-       
-    },
-    getParameter(resource,slug, params) {
-        return axiosClient.get(`${resource}/${slug}`, params);
-      },
-    post(resource,data){
-        return axiosClient.post(`${resource}`,data);
-    },
-    postAuth(resource,data,header){
-        return axiosClient.post(`${resource}`,data,header);
-    },
-    putAvatar(resource,data,header){
-        return axiosClient.put(`${resource}`,data,header);
-    },
-    put(resource,slug, params) {
-        return axiosClient.put(`${resource}/${slug}`, params);
-      },
-    setDealine(contacts,assignee){
-        const timestamp=ref('');
-        const deadline =ref('');
-        const currentTime = new Date();
-        const datenow = Math.floor(currentTime.getTime() ); 
-        contacts.value.forEach(element => {
-            
-            timestamp.value=element.created_at ;
-            
-            const millisecondsInADay = 24 * 60 * 60 * 1000;
-            const threeDaysMilliseconds = 3 * millisecondsInADay;
-            
-            const date = new Date(timestamp.value);
-            const newtimestamp = date.getTime() + threeDaysMilliseconds;
-            //so sanh time  with now
-            if(newtimestamp < datenow){
-                element.deadline="het han"
-                element.assignee=assignee;
-            }else{
-            const newdate = new Date(newtimestamp);
-            const year = newdate.getFullYear();
-            const month = newdate.getMonth() + 1; // Vì tháng được đếm từ 0 đến 11, ta cộng thêm 1
-            const day = newdate.getDate();
-            const hours = newdate.getHours();
-            deadline.value=hours + "h " + day + "-" +  month + "-" + year;
-            //set contacts
-            element.assignee=assignee;
-            element.deadline=deadline.value;
-            }
-       }); 
-    },
-    
-    getTimeCheck(res,checks){
-        const time_in=res.time_in;
-        const date= new Date(time_in);
-        const checkin =date.getHours() + ":" + date.getMinutes();
-        checks.check_in=checkin;
-        if(res.time_out!==null){
-        const time_out=res.time_out;
-        const date_out= new Date(time_out);
-        const checkout =date_out.getHours() + ":" + date_out.getMinutes();
-        checks.check_out=checkout;
-        }
-    }
-};  
+const AppService = {
+  setHeader() {
+    return (Headers = {
+      Authorization: `Bearer ${JSON.parse(JwtService.getToken())}`,
+    });
+  },
+  getTypeParameter() {
+    const url = window.location.href;
 
+    const params = new URLSearchParams(window.location.search);
+    const type = params.get("type");
+    return type;
+  },
+  get(resource, params) {
+    return axiosClient.get(`${resource}`, params);
+  },
+  getParameter(resource, slug, params) {
+    return axiosClient.get(`${resource}/${slug}`, params);
+  },
+  getNotWork(resource, year, month, header) {
+    return axiosClient.get(`${resource}/${year}/${month}`, header);
+  },
+  post(resource, data) {
+    return axiosClient.post(`${resource}`, data);
+  },
+  postAuth(resource, data, header) {
+    return axiosClient.post(`${resource}`, data, header);
+  },
+  putAvatar(resource, data, header) {
+    return axiosClient.put(`${resource}`, data, header);
+  },
+  put(resource, slug, params) {
+    return axiosClient.put(`${resource}/${slug}`, params);
+  },
+
+  setDealine(contacts, assignee) {
+    const timestamp = ref("");
+    const deadline = ref("");
+    const now = Date.now();
+
+    contacts.value.forEach((element) => {
+      timestamp.value = element.created_at;
+
+      const oneDay = 24 * 60 * 60 * 1000;
+      const threeDays = 3 * oneDay;
+
+      const date = new Date(timestamp.value);
+      const expiredAt = date.getTime() + threeDays;
+
+      //so sanh time  with now
+      if (expiredAt < now) {
+        element.deadline = "het han";
+        element.assignee = assignee;
+      } else {
+        const newDate = new Date(expiredAt);
+        const year = newDate.getFullYear();
+        const month = newDate.getMonth() + 1; // Vì tháng được đếm từ 0 đến 11, ta cộng thêm 1
+        const day = newDate.getDate();
+        const hours = newDate.getHours();
+        deadline.value = hours + "h " + day + "-" + month + "-" + year;
+        //set contacts
+        element.assignee = assignee;
+        element.deadline = deadline.value;
+      }
+    });
+  },
+
+  getTimeCheck(res, checks, color) {
+    // TODO: checkIn
+
+    // TODO: checkout
+    color.check_in = "#1dc9b7";
+    color.check_out = "#1dc9b7";
+
+    // FIXME: loi nay
+    const time_in = res.time_in;
+    const date = new Date(time_in);
+
+    const checkIn =
+      String(date.getHours()).padStart(2, "0") +
+      ":" +
+      String(date.getMinutes()).padStart(2, "0");
+    if (
+      (date.getHours() >= 9 && date.getMinutes() >= 1) ||
+      date.getHours() > 9
+    ) {
+      color.check_in = "#fd3995";
+    }
+
+    checks.check_in = checkIn;
+    if (res.time_out !== null) {
+      const time_out = res.time_out;
+      const date_out = new Date(time_out);
+
+      if (
+        date_out.getHours() < 17 ||
+        (date_out.getHours() <= 17 && date_out.getMinutes() <= 30)
+      ) {
+        color.check_out = "#fd3995";
+      }
+
+      const checkout =
+        String(date_out.getHours()).padStart(2, "0") +
+        ":" +
+        String(date_out.getMinutes()).padStart(2, "0");
+      checks.check_out = checkout;
+    }
+  },
+
+  holiday() {
+    const holiday = {
+      "2023-01-01": "Tết Dương Lịch",
+      "2023-01-02": "Nghỉ Tết Dương Lịch",
+      "2023-01-21": "Tết Nguyên Đán",
+      "2023-01-22": "Tết Nguyên Đán",
+      "2023-01-23": "Tết Nguyên Đán",
+      "2023-01-24": "Tết Nguyên Đán",
+      "2023-01-25": "Tết Nguyên Đán",
+      "2023-01-26": "Tết Nguyên Đán",
+      "2023-01-27": "Tết Nguyên Đán",
+      "2023-04-29": "Giỗ Tổ Hùng Vương",
+      "2023-04-30": "Ngày Thống nhất đất nước",
+      "2023-05-01": "Ngày Quốc tế Lao động",
+      "2023-05-02": "Nghỉ Giỗ Tổ Hùng Vương",
+      "2023-05-03": "Nghỉ Ngày Thống nhất",
+      "2023-09-02": "Ngày Quốc khánh",
+      "2023-09-04": "Nghỉ Ngày Quốc khánh",
+    };
+    return holiday;
+  },
+
+  now() {
+    const currentDate = new Date();
+
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+
+    const year = currentDate.getFullYear();
+
+    const day = currentDate.getDate().toString().padStart(2, "0");
+
+    return year + "-" + month + "-" + day;
+  },
+
+  checkButton(timeKeep, showCheckOutButton, off, holiday, dataKeys) {
+    if (timeKeep.data[timeKeep.data.length - 1].time_in !== null) {
+      showCheckOutButton.value = false;
+    }
+
+    if (
+      timeKeep.data[timeKeep.data.length - 1].time_in !== null &&
+      timeKeep.data[timeKeep.data.length - 1].time_out !== null
+    ) {
+      off.value = true;
+    }
+
+    if (dataKeys.length === 0) {
+      off.value = false;
+      showCheckOutButton.value = true;
+    }
+
+    const today = new Date().getDay();
+    const now = AppService.now();
+
+    if (today === 6 || today === 7 || Object.keys(holiday).includes(now)) {
+      off.value = true;
+    }
+  },
+};
 
 export default AppService;
-
-
- 
--- resources/js/layouts/store/url.js
+++ resources/js/layouts/store/url.js
@@ -1,43 +1,43 @@
-export const APP_URL = 'http://localhost:8080/';
-export const HOME = APP_URL + 'home';
-export const HOMECALENDAR = APP_URL + 'home/lich';
-export const LOGIN = APP_URL + 'login';
-export const REQUEST = APP_URL + 'home/request';
+export const APP_URL = "http://localhost:8080/";
+export const HOME = APP_URL + "home";
+export const HOME_CALENDAR = APP_URL + "home/lich";
+export const LOGIN = APP_URL + "login";
+export const REQUEST = APP_URL + "home/request";
 
-export const PROFILE = APP_URL + 'home/edit/profile';
+export const PROFILE = APP_URL + "home/edit/profile";
 
-export const CHANGEPASSWORD = APP_URL + 'change-password';
+export const CHANGE_PASSWORD = APP_URL + "change-password";
 
-export const APILOGIN = "api/auth/login";
+export const API_LOGIN = "api/auth/login";
 
-export const APILOGOUT = "api/auth/logout";
+export const API_LOGOUT = "api/auth/logout";
 
-export const APIREGISTER = "api/auth/register";
+export const API_REGISTER = "api/auth/register";
 
-export const APICHANGEPASSWORD = "api/auth/change-password";
+export const API_CHANGE_PASSWORD = "api/auth/change-password";
 
-export const APIMYACCOUNT = "api/users/my-account";
+export const API_MY_ACCOUNT = "api/users/my-account";
 
-export const APIREQUEST = "api/users/request";
+export const API_REQUEST = "api/users/request";
 
-export const APIUSERMNG = "api/users/request/manager";
+export const API_USER_MNG = "api/users/request/manager";
 
-export const APIREQUESTSTATUS= "api/users/member/request";
+export const API_REQUEST_STATUS = "api/users/member/request";
 
-export const APICREATEREQUEST="api/users/request/new";
+export const API_CREATE_REQUEST = "api/users/request/new";
 
-export const PATHIMAGE ="../../../../storage/";
+export const PATH_IMAGE = "../../../../storage/";
 
-export const APIAVATAR="api/users/update/avatar";
+export const API_AVATAR = "api/users/update/avatar";
 
-export const APIUPDATEACCOUNT="api/users/update/my-account"
+export const API_UPDATE_ACCOUNT = "api/users/update/my-account";
 
-export const APICREATETIMEKEEP="api/users/time-keep/new"
+export const API_CREATE_TIME_KEEP = "api/users/time-keep/new";
 
-export const APIUPDATETIMEKEEP="api/users/time-keep/update"
+export const API_UPDATE_TIME_KEEP = "api/users/time-keep/update";
 
-export const APIGETTIMEKEEP="api/users/time-keep/get"
+export const API_GET_TIME_KEEP = "api/users/time-keep/get";
 
-export const APIGETTIMEKEEPBYDAY="api/users/time-keep/getByDay"
+export const API_GET_TIME_KEEP_BY_DAY = "api/users/time-keep/getByDay";
 
-
+export const API_GET_NOT_WORK = "api/users/time-keep/getNotWork";
