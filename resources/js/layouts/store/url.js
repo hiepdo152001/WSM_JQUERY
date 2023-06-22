32111c5af9979d43639c76f07233a8bd4c14ef70
hiepdv <hiep.dv@zinza.com.vn>
2023-06-22 17:59:12 +0700

    update url api

--- resources/js/layouts/store/url.js
+++ resources/js/layouts/store/url.js
@@ -8,19 +8,30 @@ export const LOGIN = APP_URL + "login";
 
 export const REQUEST = APP_URL + "home/request";
 
-export const GET_REQUEST = APP_URL + "api/users/request/get";
-
-export const GET_USER = APP_URL + "api/users/get-user";
-
 export const DEPARTMENT = APP_URL + "home/department";
 
 export const MEMBER = APP_URL + "home/member";
 
+export const PROFILE = APP_URL + "home/edit/profile";
+
+export const STATUS_2 = "home/member/request/?type=pending";
+
+//api
+
+export const PATH_IMAGE = "../../../../storage/";
+
+export const API_AVATAR = "api/users/avatar";
+
+export const API_UPDATE_ACCOUNT = "api/users/profile";
+
+export const API_UPDATE_MEMBER = "api/users";
+
+export const GET_USER = APP_URL + "api/users";
+
+//suwa lai
 export const SEARCH = APP_URL + "api/users/search";
 
-export const AllUser = APP_URL + "api/users/gets";
-
-export const PROFILE = APP_URL + "home/edit/profile";
+export const AllUser = APP_URL + "api/users";
 
 export const CHANGE_PASSWORD = APP_URL + "change-password";
 
@@ -32,54 +43,48 @@ export const API_REGISTER = "api/auth/register";
 
 export const API_CHANGE_PASSWORD = "api/auth/change-password";
 
-export const API_MY_ACCOUNT = "api/users/my-account";
+export const API_MY_ACCOUNT = "api/users/profile";
 
-export const API_DELETE_USER = "api/users/delete/users";
+//set lai ben fe
+export const API_DELETE_USER = "api/users";
 
-export const API_ACTIVE_USER = "api/users/active/users";
+export const API_ACTIVE_USER = "api/users";
 
-export const API_REQUEST = "api/users/request";
+export const API_REQUEST = "api/users/requests/all";
 
+export const GET_REQUEST = APP_URL + "api/users/requests";
+
+//sua lai
 export const API_USER_CREATE = "api/users/request/user-create";
 
-export const API_USER_MNG = "api/users/request/manager";
+export const API_USER_MNG = "api/users/requests/manager";
 
-export const API_REQUEST_STATUS = "api/users/member/request";
+export const API_REQUEST_STATUS = "api/users/requests/member";
 
-export const API_CREATE_REQUEST = "api/users/request/new";
+export const API_CREATE_REQUEST = "api/users/requests";
 
-export const API_REQUEST_DELETE = "api/users/request/delete";
+export const API_REQUEST_DELETE = "api/users/requests";
 
-export const API_REQUEST_UPDATE = "api/users/request/update";
+export const API_REQUEST_UPDATE = "api/users/requests";
 
-export const STATUS_2 = "home/member/request/?type=pending";
+export const API_CREATE_TIME_KEEP = "api/users/time-keep/check-in";
 
-export const PATH_IMAGE = "../../../../storage/";
+export const API_UPDATE_TIME_KEEP = "api/users/time-keep/check-out";
 
-export const API_AVATAR = "api/users/update/avatar";
+export const API_GET_TIME_KEEP = "api/users/time-keep/all";
 
-export const API_UPDATE_ACCOUNT = "api/users/update/my-account";
+export const API_GET_TIME_KEEP_BY_DAY = "api/users/time-keep/by-day";
 
-export const API_UPDATE_MEMBER = "api/users/update/user";
+export const API_GET_NOT_WORK = "api/users/time-keep/not-work";
 
-export const API_CREATE_TIME_KEEP = "api/users/time-keep/new";
+export const API_ASSETS_NEW = "api/users/assets";
 
-export const API_UPDATE_TIME_KEEP = "api/users/time-keep/update";
+export const API_GET_ASSETS = "api/users/assets/me";
 
-export const API_GET_TIME_KEEP = "api/users/time-keep/get";
+export const API_GET_ASSETS_ID = "api/users/assets";
 
-export const API_GET_TIME_KEEP_BY_DAY = "api/users/time-keep/getByDay";
+export const DEPARTMENT_NEW = "api/departments";
 
-export const API_GET_NOT_WORK = "api/users/time-keep/getNotWork";
+export const DEPARTMENT_GETS = "api/departments";
 
-export const API_ASSETS_NEW = "api/users/assets/new";
-
-export const API_GET_ASSETS = "api/users/assets/get";
-
-export const API_GET_ASSETS_ID = "api/users/assets/get";
-
-export const DEPARTMENT_NEW = "api/users/department/new";
-
-export const DEPARTMENT_GETS = "api/users/department/get";
-
-export const DEPARTMENT_EDIT = "api/users/department/edit";
+export const DEPARTMENT_EDIT = "api/departments";
